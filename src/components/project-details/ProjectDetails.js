123047d6deeece98a16e0c254db13d5a4f452dc0
dejvitxc9 <dawid.uniowski@gmail.com>
2023-08-22 23:47:19 +0200

    Poprawiono linki prowadzce do Serwisu Github i struktur plików

--- src/components/project-details/ProjectDetails.js
+++ src/components/project-details/ProjectDetails.js
@@ -2,25 +2,37 @@ import "./ProjectDetails.css";
 import { FaGithubSquare } from "react-icons/fa";
 
 function ProjectDetails({ appData }) {
-  const tryMyApp = () => {window.open(appData.simulationLink, "_blank");};
-  const gitHubApp = () => {window.open(appData.gitHubLink, "_blank");};
+  const tryMyApp = () => {
+    window.open(appData.simulationLink, "_blank");
+  };
+  const gitHubApp = () => {
+    window.open(appData.gitHubLink, "_blank");
+  };
   return (
     <div className="project-details">
       <h1 className="primary-font-color">{appData.name}</h1>
       <div className="details-container">
         <div className="details-describtion">
           {appData.describtion.map((describtion, index) => {
-            return <p key={index} className="primary-font-color">{describtion}</p>;
+            return (
+              <p key={index} className="primary-font-color">
+                {describtion}
+              </p>
+            );
           })}
-          <button
-            className="btn-custom primary-font-color"
-            onClick={tryMyApp}
-          >{`Wypróbuj aplikację ${appData.name}`}</button>
-          <br/>
-          <button
-            className="btn-custom primary-font-color"
-            onClick={gitHubApp}
-          ><FaGithubSquare size={30}/>{`Repozytorium ${appData.name} w serwisie GitHub`}</button>
+          {appData.simulationLink ? (
+            <button
+              className="btn-custom primary-font-color"
+              onClick={tryMyApp}
+            >{`Wypróbuj aplikację ${appData.name}`}</button>
+          ) : (
+            <></>
+          )}
+          <br />
+          <button className="btn-custom primary-font-color" onClick={gitHubApp}>
+            <FaGithubSquare size={30} />
+            {`Repozytorium ${appData.name} w serwisie GitHub`}
+          </button>
         </div>
         <div className="details-picture-container">
           <img
